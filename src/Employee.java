73a29ff305a10085c240ba4668dc5845d6f6b97f
Stanislav <stdu92@mail.ru>
2023-03-09 20:33:27 +0300

    Курсовая работа 1

--- src/Employee.java
+++ src/Employee.java
@@ -1,11 +1,12 @@
+import java.util.Objects;
 
 public class Employee {
-    private String name;
-    private String middleName;
-    private String surname;
+    private final String name;
+    private final String middleName;
+    private final String surname;
     private int otdel;
     private int salary;
-    Counter counter;
+    private static int id = 1;
 
     public Employee(String name, String middleName, String surname, int otdel, int salary) {
         this.name = name;
@@ -41,5 +42,23 @@ public class Employee {
 
     public void setSalary(int salary) {
         this.salary = salary;
+    }
+
+    public static int getId() {
+        return id++;
+    }
+
+    @Override
+    public String toString() {
+        return "ID сотрудника " + getId() +
+                " Ф.И.О. = " + name +
+                " " + middleName +
+                " " + surname +
+                ", Отдел = " + otdel +
+                ", Зарплата = " + salary;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, middleName, surname, otdel, salary, id);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,72 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Employee[] employee = new Employee[10];
+        employee[0] = new Employee("Иван", "Иванович", "Иванов", 1, 30000);
+        employee[1] = new Employee("Сергей", "Петрович", "Петров", 1, 32000);
+        employee[2] = new Employee("Петр", "Николаевич", "Сидоров", 1, 31000);
+        employee[3] = new Employee("Федор", "Петрович", "Рыжов", 2, 44000);
+        employee[4] = new Employee("Николай", "Сергеевич", "Смирнов", 2, 44000);
+        employee[5] = new Employee("Сергей", "Максимович", "Попов", 4, 32000);
+        employee[6] = new Employee("Станислав", "Александрович", "Кузнецов", 3, 25000);
+        employee[7] = new Employee("Александр", "Геннвдьевич", "Соколов", 3, 26000);
+        employee[8] = new Employee("Евгений", "Петрович", "Лебедев", 5, 51000);
+        employee[9] = new Employee("Иван", "Викторович", "Васильев", 5, 55000);
+        /*1*/
+        System.out.println("a. Список всех сотрудников ");
+        for (int i = 0; i < employee.length; i++) {
+            System.out.println(employee[i]);
+        }
+
+        /*2*/
+        int sum = 0;
+        for (int i = 0; i < employee.length; i++) {
+            sum = sum + employee[i].getSalary();
+        }
+        System.out.println("b. Сумма затрат на зарплаты в месяц = " + sum);
+        /*3*/
+        int min = employee[0].getSalary();
+        String minSalary = null;
+        for (int i = 0; i < employee.length; i++) {
+            if (min > employee[i].getSalary()) {
+                min = employee[i].getSalary();
+            }
+        }
+        for (int i = 0; i < employee.length; i++) {
+            if (min == employee[i].getSalary()) {
+                minSalary = employee[i].getName() + " " + employee[i].getMiddleName() + " " + employee[i].getSurname();
+            }
+        }
+        System.out.println("с. Сотрудник с минимальной зарплатой: " + minSalary + " (" + min + ") ");
+
+        /*4*/
+        int max = employee[0].getSalary();
+        String maxSalary = null;
+        for (int i = 0; i < employee.length; i++) {
+            if (max < employee[i].getSalary()) {
+                max = employee[i].getSalary();
+            }
+        }
+        for (int i = 0; i < employee.length; i++) {
+            if (max == employee[i].getSalary()) {
+                maxSalary = employee[i].getName() + " " + employee[i].getMiddleName() + " " + employee[i].getSurname();
+            }
+        }
+        System.out.println("d. Сотрудник с максимальной зарплатой: " + maxSalary + " (" + max + ") ");
+
+        /*5*/
+        int middleSum = 0;
+        middleSum = sum / employee.length;
+        System.out.println("e. Среднее значение зарплат = " + middleSum);
+
+        /*6*/
+        System.out.println("f. Список всех сотрудников: ");
+        for (int i = 0; i < employee.length; i++) {
+            System.out.println(employee[i].getName() + " " + employee[i].getMiddleName() + " " + employee[i].getSurname());
+        }
 
     }
+
+
 }
